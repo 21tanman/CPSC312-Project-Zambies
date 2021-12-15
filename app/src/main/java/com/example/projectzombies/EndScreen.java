bff6db8a3d9d4bab53b35afa5f56f6c6c35defbf
21tanman <tannerafranz@gmail.com>
2021-12-15 10:46:36 -0800

    Adding shared preferences

--- app/src/main/java/com/example/projectzombies/EndScreen.java
+++ app/src/main/java/com/example/projectzombies/EndScreen.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EndScreen extends AppCompatActivity {
     @Override
@@ -12,8 +14,14 @@ public class EndScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
 
-        Context context = getActivity();
-        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.integer.preference_file_key),Context.MODE_PRIVATE);
+        //Context context = getActivity();
+        //SharedPreferences sharedPref = context.getSharedPreferences(getString(R.integer.preference_file_key),Context.MODE_PRIVATE);
 
+        final Button button = (Button) findViewById(R.id.playAgainBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your handler code here
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projectzombies/StartScreen.java
+++ app/src/main/java/com/example/projectzombies/StartScreen.java
@@ -3,6 +3,8 @@ package com.example.projectzombies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        final Button button = findViewById(R.id.playBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                GameActivity newGame = new GameActivity();
+            }
+        });
     }
+
 }
