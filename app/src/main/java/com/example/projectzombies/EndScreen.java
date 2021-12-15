d4cbbb5edd499691fb441607ce365ead5573da00
21tanman <tannerafranz@gmail.com>
2021-12-15 10:33:55 -0800

    Adding shared preferences

--- app/src/main/java/com/example/projectzombies/EndScreen.java
+++ app/src/main/java/com/example/projectzombies/EndScreen.java
@@ -2,6 +2,8 @@ package com.example.projectzombies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class EndScreen extends AppCompatActivity {
@@ -9,5 +11,9 @@ public class EndScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
+
+        Context context = getActivity();
+        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.integer.preference_file_key),Context.MODE_PRIVATE);
+
     }
 }
